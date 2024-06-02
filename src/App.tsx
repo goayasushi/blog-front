9f2508f4b93ffe9747483451141efa52a38b9610
goayasushi <goayasushi@gmail.com>
2024-06-02 11:08:19 +0900

    疎通確認

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import parse, { DOMNode, Element as DomElement } from "html-react-parser";
+
+import "./App.css";
+import { client } from "./libs/client";
+
+type Blog = {
+  category: {
+    createdAt: string;
+    id: string;
+    name: string;
+    publishedAt: string;
+    revisedAt: string;
+    updatedAt: string;
+  };
+  content: string;
+  createdAt: string;
+  eyecatch: {
+    url: string;
+    height: number;
+    width: number;
+  };
+  id: string;
+  publishedAt: string;
+  revisedAt: string;
+  title: string;
+  updatedAt: string;
+};
 
 function App() {
+  const [blogs, setBlogs] = useState<Array<Blog>>([]);
+
+  useEffect(() => {
+    client
+      .get({
+        endpoint: "blogs",
+      })
+      .then((res) => {
+        console.log(res.contents);
+        setBlogs(res.contents);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const transform = (node: DOMNode) => {
+    if (node.type === "tag" && (node as DomElement).name === "img") {
+      const imgNode = node as DomElement;
+      return (
+        <img
+          src={imgNode.attribs.src}
+          alt={imgNode.attribs.alt}
+          style={{ width: "100%", height: "auto", border: "2px solid red" }}
+        />
+      );
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>疎通確認</p>
+      {blogs.map((blog) => (
+        <div key={blog.id}>
+          <p>{blog.title}</p>
+          <img src={blog.eyecatch.url} style={{ width: 600, height: 300 }} />
+          <br />
+          <div>{parse(blog.content, { replace: transform })}</div>
+        </div>
+      ))}
     </div>
   );
 }
---
+++ src/libs/client.js
@@ -0,0 +1,6 @@
+import { createClient } from "microcms-js-sdk";
+
+export const client = createClient({
+  serviceDomain: "yowaken",
+  apiKey: "snE03daYPOB0fe6HWoYFtkIdMKpWKk2iRt2W",
+});
