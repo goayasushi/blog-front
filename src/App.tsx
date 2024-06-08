aac46477e22ea265957d19ea7728dfdc6e440cd6
goayasushi <goayasushi@gmail.com>
2024-06-08 11:45:13 +0900

    サイト全体のルーティング設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,13 @@
-import { useEffect, useState } from "react";
-import parse, { DOMNode, Element as DomElement } from "html-react-parser";
-
 import "./App.css";
-import { client } from "./libs/client";
-import { Header } from "./components/organisms/Header";
+import { RouterProvider } from "react-router-dom";
 
-type Blog = {
-  category: {
-    createdAt: string;
-    id: string;
-    name: string;
-    publishedAt: string;
-    revisedAt: string;
-    updatedAt: string;
-  };
-  content: string;
-  createdAt: string;
-  eyecatch: {
-    url: string;
-    height: number;
-    width: number;
-  };
-  id: string;
-  publishedAt: string;
-  revisedAt: string;
-  title: string;
-  updatedAt: string;
-};
+import { router } from "./router/Router";
 
 function App() {
-  const [blogs, setBlogs] = useState<Array<Blog>>([]);
-
-  useEffect(() => {
-    client
-      .get({
-        endpoint: "blogs",
-      })
-      .then((res) => {
-        console.log(res.contents);
-        setBlogs(res.contents);
-      })
-      .catch((err) => console.log(err));
-  }, []);
-
-  const transform = (node: DOMNode) => {
-    if (node.type === "tag" && (node as DomElement).name === "img") {
-      const imgNode = node as DomElement;
-      return (
-        <img
-          src={imgNode.attribs.src}
-          alt={imgNode.attribs.alt}
-          style={{ width: "100%", height: "auto", border: "2px solid red" }}
-        />
-      );
-    }
-  };
-
   return (
-    <div className="App">
-      <Header />
-      <p>疎通確認</p>
-      {blogs.map((blog) => (
-        <div key={blog.id}>
-          <p>{blog.title}</p>
-          <img src={blog.eyecatch.url} style={{ width: 600, height: 300 }} />
-          <br />
-          <div>{parse(blog.content, { replace: transform })}</div>
-        </div>
-      ))}
-    </div>
+    <>
+      <RouterProvider router={router} />
+    </>
   );
 }
 
