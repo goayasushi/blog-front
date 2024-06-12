29b09217d2bdb518618a99a4e2b44a80d968819b
goayasushi <goayasushi@gmail.com>
2024-06-12 09:00:31 +0900

    globalなstyle設定

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,14 @@ import { ChakraProvider } from "@chakra-ui/react";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import App from "./App";
+import theme from "./theme/theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
