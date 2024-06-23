9370dbc0f87d6f702a79c080bda780bcc19a5189
goayasushi <goayasushi@gmail.com>
2024-06-23 15:12:45 +0900

    記事詳細　データ取得確認

--- src/components/pages/Article.tsx
+++ src/components/pages/Article.tsx
@@ -1,5 +1,73 @@
-import { FC, memo } from "react";
+import { FC, memo, useEffect, useState } from "react";
+import { client } from "../../libs/client";
+import parse, { DOMNode, Element as DomElement } from "html-react-parser";
+
+type Blog = {
+  category: {
+    createdAt: string;
+    id: string;
+    name: string;
+    publishedAt: string;
+    revisedAt: string;
+    updatedAt: string;
+  };
+  content: string;
+  createdAt: string;
+  eyecatch: {
+    url: string;
+    height: number;
+    width: number;
+  };
+  id: string;
+  publishedAt: string;
+  revisedAt: string;
+  title: string;
+  updatedAt: string;
+};
 
 export const Article: FC = memo(() => {
-  return <div>記事詳細ページ</div>;
+  const [blogs, setBlogs] = useState<Array<Blog>>([]);
+
+  useEffect(() => {
+    client
+      .get({
+        endpoint: "blogs",
+      })
+      .then((res) => {
+        console.log(res.contents);
+        setBlogs(res.contents);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const transform = (node: DOMNode) => {
+    if (node.type === "tag" && (node as DomElement).name === "img") {
+      const imgNode = node as DomElement;
+      return (
+        <img
+          src={imgNode.attribs.src}
+          alt={imgNode.attribs.alt}
+          style={{ width: "100%", height: "auto", border: "2px solid red" }}
+        />
+      );
+    }
+  };
+
+  return (
+    <>
+      <p>疎通確認</p>
+      {blogs.map((blog) => (
+        <div key={blog.id}>
+          <p>{blog.title}</p>
+          <img
+            src={blog.eyecatch.url}
+            style={{ width: 600, height: 300 }}
+            alt={blog.title}
+          />
+          <br />
+          <div>{parse(blog.content, { replace: transform })}</div>
+        </div>
+      ))}
+    </>
+  );
 });
