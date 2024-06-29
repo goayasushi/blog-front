224a34006be212786718c253f0a4e686e480382f
goayasushi <goayasushi@gmail.com>
2024-06-29 11:11:45 +0900

    カテゴリー一覧　作成

--- src/components/pages/CategoryList.tsx
+++ src/components/pages/CategoryList.tsx
@@ -1,5 +1,38 @@
-import { FC, memo } from "react";
+import { FC, memo, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import { Article } from "../../types/article";
+import { client } from "../../libs/client";
+import { Box } from "@chakra-ui/react";
+import { Breadcrumbs } from "../molecules/Breadcrumbs";
+import { ArticleList } from "../organisms/article/ArticleList";
 
 export const CategoryList: FC = memo(() => {
-  return <div>カテゴリー一覧ページ</div>;
+  const [articles, setArticles] = useState<Array<Article>>([]);
+  const { id } = useParams<{ id: string }>();
+
+  useEffect(() => {
+    client
+      .get({
+        endpoint: "blogs",
+        queries: { filters: `category[equals]${id}` },
+      })
+      .then((res) => {
+        setArticles(res.contents);
+        window.scrollTo(0, 0);
+      })
+      .catch((err) => console.log(err));
+  }, [id]);
+
+  return (
+    <Box px={{ base: 4, md: 10 }}>
+      <Box mb={4}>
+        <Breadcrumbs
+          category={articles[0]?.category.name}
+          categoryId={articles[0]?.category.id}
+        />
+      </Box>
+      <ArticleList articles={articles} baseColumns={1} mdColumns={2} />
+    </Box>
+  );
 });
