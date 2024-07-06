187f219510ae97abb4395ef7d7cfa9e3e3019744
goayasushi <goayasushi@gmail.com>
2024-07-06 10:12:51 +0900

    react query導入

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import App from "./App";
 import theme from "./theme/theme";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
