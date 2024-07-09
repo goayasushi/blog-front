c771c47adf5c8410ff73374fb3b61f7a12ec9a35
goayasushi <goayasushi@gmail.com>
2024-07-09 21:36:28 +0900

    環境変数設定

--- src/libs/client.ts
+++ src/libs/client.ts
@@ -2,5 +2,5 @@ import { createClient } from "microcms-js-sdk";
 
 export const client = createClient({
   serviceDomain: "yowaken",
-  apiKey: "snE03daYPOB0fe6HWoYFtkIdMKpWKk2iRt2W",
+  apiKey: process.env.REACT_APP_API_KEY || "",
 });
