ceed3b8f3e544f9d10e28ba0c772827c443aa666
goayasushi <goayasushi@gmail.com>
2024-07-13 17:29:46 +0900

    カテゴリ取得エラハン　コードによる分岐追加

--- src/components/errors/CategoryError.tsx
+++ src/components/errors/CategoryError.tsx
@@ -11,21 +11,37 @@ type Props = {
   error: Error;
 };
 
+const is4xxrError = (status: number) => {
+  return status >= 400 && status < 500;
+};
+
 export const CategoryError: FC<Props> = memo((props) => {
   const { error } = props;
-  return (
-    <>
-      <Alert status="error">
-        <AlertIcon />
-        <Box>
-          <AlertTitle>カテゴリーの取得に失敗しました。</AlertTitle>
-          <AlertDescription>
-            時間を置いて再度お試しください。
-            <br />
-            {error.message}
-          </AlertDescription>
-        </Box>
-      </Alert>
-    </>
-  );
+  const errorMessage = error.message;
+
+  let statusCode = 0;
+  const match = error.message.match(/status: (\d+)/);
+  if (match) {
+    statusCode = parseInt(match[1], 10);
+  }
+
+  if (is4xxrError(statusCode)) {
+    return (
+      <>
+        <Alert status="error">
+          <AlertIcon />
+          <Box>
+            <AlertTitle>カテゴリーの取得に失敗しました。</AlertTitle>
+            <AlertDescription>
+              時間を置いて再度お試しください。
+              <br />
+              {errorMessage}
+            </AlertDescription>
+          </Box>
+        </Alert>
+      </>
+    );
+  } else {
+    throw error;
+  }
 });
