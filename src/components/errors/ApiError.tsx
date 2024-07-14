a98c0850850b03639d9fc14b4ccfda4de431cc30
goayasushi <goayasushi@gmail.com>
2024-07-14 11:03:01 +0900

    コンポーネントエラー　リセット関数追加

--- src/components/errors/ApiError.tsx
+++ src/components/errors/ApiError.tsx
@@ -4,11 +4,13 @@ import {
   AlertDescription,
   AlertIcon,
   AlertTitle,
-  Box,
+  Button,
 } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
   error: Error;
+  resetErrorBoundary: () => void;
 };
 
 const is4xxrError = (status: number) => {
@@ -16,7 +18,13 @@ const is4xxrError = (status: number) => {
 };
 
 export const ApiError: FC<Props> = memo((props) => {
-  const { error } = props;
+  const { error, resetErrorBoundary } = props;
+  const navigate = useNavigate();
+
+  const onClickReset = () => {
+    resetErrorBoundary();
+    navigate("/");
+  };
 
   let statusCode = 0;
   const match = error.message.match(/status: (\d+)/);
@@ -27,32 +35,50 @@ export const ApiError: FC<Props> = memo((props) => {
   if (statusCode === 404) {
     return (
       <>
-        <Alert status="error">
+        <Alert
+          status="error"
+          flexDirection="column"
+          justifyContent="center"
+          textAlign="center"
+        >
           <AlertIcon />
-          <Box>
-            <AlertTitle>ページが見つかりません</AlertTitle>
-            <AlertDescription>
-              お探しのページは存在しないか、削除された可能性があります。
-              <br />
-              {error.message}
-            </AlertDescription>
-          </Box>
+          <AlertTitle>ページが見つかりません</AlertTitle>
+          <AlertDescription>
+            お探しのページは存在しないか、削除された可能性があります。
+            <br />
+            {error.message}
+          </AlertDescription>
+          <AlertDescription mt={4}>
+            再開するには、以下のボタンをクリックしてください。
+          </AlertDescription>
+          <Button mt={4} colorScheme="gray" onClick={onClickReset}>
+            トップ画面へ戻る
+          </Button>
         </Alert>
       </>
     );
   } else if (is4xxrError(statusCode)) {
     return (
       <>
-        <Alert status="error">
+        <Alert
+          status="error"
+          flexDirection="column"
+          justifyContent="center"
+          textAlign="center"
+        >
           <AlertIcon />
-          <Box>
-            <AlertTitle>エラーが発生しました</AlertTitle>
-            <AlertDescription>
-              リクエストに問題がありました。時間を置いて再度お試しください。
-              <br />
-              {error.message}
-            </AlertDescription>
-          </Box>
+          <AlertTitle>エラーが発生しました</AlertTitle>
+          <AlertDescription>
+            リクエストに問題がありました。時間を置いて再度お試しください。
+            <br />
+            {error.message}
+          </AlertDescription>
+          <AlertDescription mt={4}>
+            再開するには、以下のボタンをクリックしてください。
+          </AlertDescription>
+          <Button mt={4} colorScheme="gray" onClick={onClickReset}>
+            トップ画面へ戻る
+          </Button>
         </Alert>
       </>
     );
